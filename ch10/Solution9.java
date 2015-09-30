542e4899d826bef36b82fac3cff2d76afea80e14
prabhsimransingh <prabhsis@andrew.cmu.edu>
2015-09-30 01:55:15 -0400

    Update and rename Solution8.java to Solution9.java

--- ch10/Solution9.java
+++ ch10/Solution9.java
@@ -1,8 +1,8 @@
 /*
-This file: Solution8.java
+This file: Solution9.java
 Programmer: Prabh Simran Singh (prabhsis@andrew.cmu.edu)
 Course/Section: 11-601
-Assignment: Chapter10 - 8
+Assignment: Chapter10 - 9
 Description: Given a M*N matrix sorted in ascending order , weite method to find an element.
 Assumption:
 Time Complexity: O(logn)
@@ -10,7 +10,7 @@ Space Complexity: O(1)
 Last Modified: 9/30/2015 
  */
 
-public class Solution8 {
+public class Solution9 {
 	public static void main(String[] args) {
 		int[][] matrix = { { 1, 3 }, { 2, 4 } };
 		System.out.println(searchMatrix(matrix,3));
