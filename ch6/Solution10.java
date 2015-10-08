041dc5cd57b6e5b12439f065178850dd79c3170e
prabhsimransingh <prabhsis@andrew.cmu.edu>
2015-10-08 03:09:46 -0400

    Create Solution10.java

---
+++ ch6/Solution10.java
@@ -0,0 +1,15 @@
+/*
+This file: Solution10.java
+Programmer: Prabh Simran Singh (prabhsis@andrew.cmu.edu)
+Course/Section: 11-601
+Assignment: Chapter6 - 10
+Description: Poision
+Last Modified: 10/8/2015 
+ */
+//Solution 1 - we Divide the bottles across 10 strips in groups of 100. then we get results back in seven days
+// one group will be positive again divide that group and repeat until one bottle is left take 28 days
+// Solution 2 - Divide bottle into 10 groups then day 7 reveals the group, we can divide the strip and test to map to a
+// particular group such that strip 0 only test 0XX bottle numbers and x0x bottle from day 1,8 and so on.
+public class Solution10 {
+
+}
